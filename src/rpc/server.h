1d5ab8f06cc8fc04bdbaddf513d495cd3d4c2db5
biscuit <biscuit@biscuits-iMac.gateway>
2019-06-08 12:54:56 +0800

    add makekeypair rpc command

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -211,6 +211,7 @@ extern UniValue getblocktemplate(const UniValue& params, bool fHelp);
 extern UniValue submitblock(const UniValue& params, bool fHelp);
 extern UniValue estimatefee(const UniValue& params, bool fHelp);
 extern UniValue estimatepriority(const UniValue& params, bool fHelp);
+extern UniValue makekeypair(const UniValue& params, bool fHelp);
 
 extern UniValue getnewaddress(const UniValue& params, bool fHelp); // in rpcwallet.cpp
 extern UniValue getaccountaddress(const UniValue& params, bool fHelp);
