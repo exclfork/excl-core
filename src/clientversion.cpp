214e5115a0831e01ec31a26b036719c1f0a6d063
bumbacoin <bumbacoin@gmail.com>
2019-10-01 17:15:46 +0800

    update client name

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both excld and excl-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("EXCL Core");
+const std::string CLIENT_NAME("EXCL Core - Boutique");
 
 /**
  * Client version number
